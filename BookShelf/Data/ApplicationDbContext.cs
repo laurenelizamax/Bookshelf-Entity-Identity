57675acebc50b680f712dda7d37701ae2ebd68e7
Lauren <laurenelizamax@icloud.com>
2020-01-21 14:55:07 -0600

    initial commit

---
+++ BookShelf/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookShelf.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShelf.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+
+
+    }
+}
---
+++ BookShelf/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShelf.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Author")]
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+
+        [Display(Name = "Year Published")]
+        public int YearPublished { get; set; }
+        public int Rating { get; set; }
+        public string Genre { get; set; }
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
